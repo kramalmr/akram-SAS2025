dea12e05b24f4038968776429e641fe4db00b85f
kramalmr <akram3107@student.abudzar.sch.id>
2025-06-05 10:09:12 +0700

    SAS

---
+++ src/SAS.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-6xl font-bold">Akram SAS 2025</h1>
+      <p className="text-2xl mt-4">
+        This is a simple React application using Vite and Tailwind CSS.
+      </p>
+      <img
+        className="w-64 mt-8"
+        src="https://res.cloudinary.com/dri8yyakb/image/upload/v1664474361/sas-logo_ay8qka.png"
+        alt="SAS logo"
+      />
+    </div>
+  );
+}
