1878b488ddb2bb16a7cb4810bdc5a099b9172e7a
kramalmr <akram3107@student.abudzar.sch.id>
2025-06-05 10:53:50 +0700

    Update SAS.jsx

--- src/SAS.jsx
+++ src/SAS.jsx
@@ -2,8 +2,8 @@ export default function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-6xl font-bold mt-24">Akram SAS 2025</h1>
-      <p className="text-sm mt-4 px-32 text-center">
-        TUGAS AKRAM SAS 2025 - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vel architecto debitis dolore, aliquam pariatur eum natus ipsum odio quas dolorem, soluta aut quam, provident earum minima eligendi? Unde iure voluptas beatae dolorum quo voluptates tempore perspiciatis voluptatibus quas laudantium doloribus repudiandae quos ducimus iusto similique aliquid reprehenderit natus, a accusamus nihil aperiam numquam soluta. Nisi, ducimus. Accusantium vel quasi aliquid error vero. Veritatis dolor qui facere possimus fugiat, recusandae officiis aut sequi deleniti beatae nihil dolorum laboriosam neque, animi molestias tempora. Hic aspernatur laboriosam suscipit sapiente tempore nostrum nesciunt enim veniam, autem corrupti doloremque quod maiores recusandae eos quibusdam.
+      <p className="text-sm my-5 px-32 text-center">
+        AKRAM SAS 2025 - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vel architecto debitis dolore, aliquam pariatur eum natus ipsum odio quas dolorem, soluta aut quam, provident earum minima eligendi? Unde iure voluptas beatae dolorum quo voluptates tempore perspiciatis voluptatibus quas laudantium doloribus repudiandae quos ducimus iusto similique aliquid reprehenderit natus, a accusamus nihil aperiam numquam soluta. Nisi, ducimus. Accusantium vel quasi aliquid error vero. Veritatis dolor qui facere possimus fugiat, recusandae officiis aut sequi deleniti beatae nihil dolorum laboriosam neque, animi molestias tempora. Hic aspernatur laboriosam suscipit sapiente tempore nostrum nesciunt enim veniam, autem corrupti doloremque quod maiores recusandae eos quibusdam.
       </p>
       <img
         className="w-64"
